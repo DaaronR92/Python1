ce5a447fa464fcc5704eb8c88cfa4054fc4cbc46
David <85143249+DaaronR92@users.noreply.github.com>
2024-08-24 22:51:08 -0400

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,23 @@
+print("Today ia a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "David"
+
+print(greeting + name)
+
+age = 32
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + f" is {age} years old")
+print(type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
