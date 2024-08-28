12df955091f69c51f83112cd14a2ae239db1647c
David <85143249+DaaronR92@users.noreply.github.com>
2024-08-27 22:17:28 -0400

    Add files via upload

---
+++ sliceback.py
@@ -0,0 +1,23 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters
+
+print(letters[16:13:-1])
+print()
+
+print(letters[4::-1])
+print()
+
+print(letters[:-9:-1])
+print()
+
+print(letters[-4:])
+print()
+
+print(letters[-1:])
+print()
+
+print(letters[:1])
+print()
+
+print(letters[0])
